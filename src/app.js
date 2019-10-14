0e8fdff480628a6863333fd62bf76357470440a0
Willian Rodelo <wrodelogarcia@gmail.com>
2019-10-13 23:34:22 -0500

    cors add

--- src/app.js
+++ src/app.js
@@ -10,6 +10,14 @@ var opctions = {
 app.set('port',process.env.PORT || 1337);
 app.use(express.json());
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 app.use(require('./routes/apiphotoserver'));
 
 app.listen(app.get('port'), ()=>{
